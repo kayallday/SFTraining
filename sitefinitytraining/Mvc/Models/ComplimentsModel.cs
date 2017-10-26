ef4a4fe98b90797ba6a353f59c9312fbf9aaa1d7
KayBeard <kbeard@apterainc.com>
2017-10-26 14:21:30 -0400

    Created the Complimentor

---
+++ sitefinitytraining/Mvc/Models/ComplimentsModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitefinityWebApp.Mvc.Models
+{
+    public class ComplimentsModel
+    {
+        private string _compliments;
+
+        public string Compliments
+        {
+            get
+            {
+                return _compliments;
+            }
+            set
+            {
+                _compliments = value;
+            }
+        }
+        private string[] _complimentsArray => _compliments.Split(',').Select(c => c.Trim()).ToArray();
+        public ComplimentsModel()
+        {
+            _compliments = "You're the best... AROUNDDD! Nothing's gonna ever bring you down!.";
+        }
+        public ComplimentsModel (string compliments)
+        {
+            this._compliments = compliments;
+        }
+        public string ComplimentMe()
+        {
+            if (String.IsNullOrEmpty(_compliments))
+                return "Default Compliment";
+            Random rnd = new Random();
+            int r = rnd.Next(_complimentsArray.Count());
+            return _complimentsArray[r];
+        }
+    }
+}
